7706bbfc7c3218b3725db7f2f3b7f0a2df7bc132
Eric Onyango <eric.onyango@student.moringaschool.com>
2022-12-03 22:12:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name: "Yves",
+    streetAddress: "SauntL",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return Object.assign({}, employee, {
+         [key]: value });
+  }
+  
+  const destructivelyUpdateEmployeeWithKeyAndValue = function(employee, key, value) {
+    employee[key] = value;
+  
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const copyOfDriver = Object.assign({}, employee);
+  
+    delete copyOfDriver[key];
+  
+    return copyOfDriver;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+  
+    return employee;
+  }
